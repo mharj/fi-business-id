2622334f89786bffed2a2dd438deacb7a26cd1d0
Marko Harjula <marko.harjula@avanio.com>
2025-03-28 06:54:47 +0200

    dev updates, jsdoc fixes, types and better handling for errors

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,37 @@ const businessIdPattern = /^\d{6,7}-\d$/;
 const idPattern = /^\d{7}$/;
 
 /**
+ * Finnish Business ID (Y-tunnus) type.
+ * @template Brand Optional brand type for a more strict type (e.g., use Zod branded type or custom type).
+ * @since v0.1.0
+ */
+export type FiBusinessId<Brand = `${number}-${number}`> = Brand & `${number}-${number}`;
+
+/**
+ * Checks if the given value is of type FiBusinessId.
+ * @param {unknown} value The value to check.
+ * @returns {value is FiBusinessId} True if the value is of type FiBusinessId, false otherwise.
+ * @since v0.1.0
+ */
+export function isFiBusinessIdType(value: unknown): value is FiBusinessId {
+	return typeof value === 'string' && businessIdPattern.test(value);
+}
+
+/**
+ * Checks if the given value is a valid business ID without the checksum.
+ * @param {unknown} value The value to check.
+ * @returns {boolean} True if the value is a valid business ID without the checksum, false otherwise.
+ * @since v0.1.0
+ */
+export function isFiBusinessIdBaseType(value: unknown): value is `${number}` {
+	return typeof value === 'string' && idPattern.test(value);
+}
+/**
  * Gets the multiplier for the given index.
- * @param index The index for which to get the multiplier.
- * @returns The multiplier for the given index.
+ * @param {number} index The index for which to get the multiplier.
+ * @returns {number} The multiplier for the given index.
  * @throws Will throw a RangeError if the index is out of bounds.
+ * @since v0.0.6
  */
 export function getMultiplier(index: number): number {
 	const multiplier = multipliers[index];
@@ -19,8 +46,8 @@ export function getMultiplier(index: number): number {
 
 /**
  * Pads the given value to the next multiple of 11.
- * @param value The value to pad.
- * @returns The padded value.
+ * @param {number} value The value to pad.
+ * @returns {number} The padded value.
  */
 function pad(value: number): number {
 	return Math.ceil(value / 11) * 11 - value;
@@ -28,8 +55,8 @@ function pad(value: number): number {
 
 /**
  * Calculates the checksum for the given business ID data.
- * @param data An array of numbers representing the business ID.
- * @returns The checksum value.
+ * @param {number[]} data An array of numbers representing the business ID.
+ * @returns {number} The checksum value.
  */
 function buildCheckSum(data: number[]): number {
 	const sum = data.reduce((prev, value, idx) => prev + getMultiplier(idx) * value, 0);
@@ -38,50 +65,68 @@ function buildCheckSum(data: number[]): number {
 
 /**
  * Normalizes the business ID by adding a leading zero if it has six digits.
- * @param businessId The business ID to normalize.
- * @returns The normalized business ID.
+ * @param {string} businessId The business ID to normalize.
+ * @returns {string} The normalized business ID.
  */
-function normalizeBusinessId(businessId: string): string {
-	if (getBaseId(businessId).length === 6) {
-		return '0' + businessId;
-	}
-	return businessId;
+function normalizeBusinessId(businessId: FiBusinessId): FiBusinessId {
+	return businessId.padStart(9, '0') as FiBusinessId;
+}
+
+/**
+ * Builds an Error object for an invalid business ID.
+ * @param {unknown} value The value that is not a valid business ID.
+ * @returns {Error} The Error object.
+ * @since v0.1.0
+ */
+function buildValidationError(value: unknown): Error {
+	return new Error(`${JSON.stringify(value)} is not valid Finnish Business ID`);
 }
 
 /**
  * Gets the base ID from the given business ID without the checksum.
- * @param businessId The business ID from which to get the base ID.
- * @returns The base ID without the checksum.
+ * @param {string} businessId The business ID from which to get the base ID.
+ * @returns {string} The base ID without the checksum.
+ * @throws Will throw an Error if the business ID is not valid.
+ * @since v0.0.6
  */
-export function getBaseId(businessId: string): string {
+export function getBaseId(businessId: FiBusinessId): string {
 	const baseId = businessId.split('-')[0];
 	if (!baseId) {
-		throw new Error('not valid business id');
+		throw buildValidationError(businessId);
 	}
 	return baseId;
 }
 
 /**
  * Builds a business ID by appending a checksum to the given ID data.
- * @param idData The base ID data.
- * @returns The complete business ID with checksum.
+ * @template Brand Optional brand type for a more strict type (e.g., use Zod branded type or custom type).
+ * @param {string | number | null | undefined} idData The base ID data.
+ * @returns {string} The complete business ID with checksum.
+ * @throws Will throw an Error if the ID data is not valid.
  * @since v0.0.1
  */
-export function buildBusinessId(idData: string): string {
-	if (!idData.match(idPattern)) {
-		throw new Error('not valid business id');
+export function buildBusinessId<Brand = `${number}-${number}`>(idData: string | number | null | undefined): FiBusinessId<Brand> {
+	if (typeof idData === 'string' && idData.length !== 7) {
+		idData = idData.padStart(7, '0');
 	}
-	return `${idData}-${buildCheckSum(idData.split('').map((c) => parseInt(c, 10))).toString()}`;
+	if (typeof idData === 'number') {
+		idData = idData.toString().padStart(7, '0');
+	}
+	if (!isFiBusinessIdBaseType(idData)) {
+		throw buildValidationError(idData);
+	}
+	return `${idData}-${buildCheckSum(idData.split('').map((c) => parseInt(c, 10))).toString()}` as FiBusinessId<Brand>;
 }
 
 /**
  * Validates the given business ID.
- * @param businessId The business ID to validate.
- * @returns True if the business ID is valid, false otherwise.
+ * @template Brand Optional brand type for a more strict type (e.g., use Zod branded type or custom type).
+ * @param {string} businessId The business ID to validate.
+ * @returns {string} True if the business ID is valid, false otherwise.
  * @since v0.0.1
  */
-export function isValidBusinessId(businessId: string): boolean {
-	if (!businessId.match(businessIdPattern)) {
+export function isValidBusinessId<Brand = `${number}-${number}`>(businessId: string | null | undefined): businessId is FiBusinessId<Brand> {
+	if (!isFiBusinessIdType(businessId)) {
 		return false;
 	}
 	const normalizedId = normalizeBusinessId(businessId);
--- test/validation.test.ts
+++ test/validation.test.ts
@@ -1,17 +1,30 @@
-import {buildBusinessId, getBaseId, getMultiplier, isValidBusinessId} from '../src/';
 import {describe, expect, it} from 'vitest';
+import {buildBusinessId, type FiBusinessId, getBaseId, getMultiplier, isValidBusinessId} from '../src';
+
+type CustomBrand = {__T: 'FiBusinessId'};
 
 describe('test business ID', () => {
 	describe('test build business ID', () => {
 		it('should build business ID', () => {
+			const _value: FiBusinessId<CustomBrand> = buildBusinessId<CustomBrand>('1572860');
 			expect(buildBusinessId('1572860')).to.be.eq('1572860-0');
+			expect(buildBusinessId(0)).to.be.eq('0000000-0');
+			expect(buildBusinessId('0')).to.be.eq('0000000-0');
 		});
 		it('should fail to build broken business ID', () => {
-			expect(buildBusinessId.bind(null, 'ASD')).to.throw(Error, 'not valid business id');
+			expect(() => buildBusinessId('ASD')).to.throw(Error, 'is not valid Finnish Business ID');
+			expect(buildBusinessId.bind(null, -1)).to.throw(Error, 'is not valid Finnish Business ID');
+			expect(buildBusinessId.bind(null, 10000000)).to.throw(Error, 'is not valid Finnish Business ID');
+			expect(buildBusinessId.bind(null, null)).to.throw(Error, 'is not valid Finnish Business ID');
 		});
 	});
 	describe('test validate business ID', () => {
 		it('should validate business ID', () => {
+			const value: string = '1572860-0';
+			const isValid = isValidBusinessId<CustomBrand>(value);
+			if (isValid) {
+				const _test: FiBusinessId<CustomBrand> = value;
+			}
 			expect(isValidBusinessId('1572860-0')).to.be.eq(true);
 			expect(isValidBusinessId('1572860-1')).to.be.eq(false);
 			expect(isValidBusinessId('0737546-2')).to.be.eq(true);
@@ -20,6 +33,8 @@ describe('test business ID', () => {
 			expect(isValidBusinessId('1456344-5')).to.be.eq(true);
 			expect(isValidBusinessId('2729069-3')).to.be.eq(true);
 			expect(isValidBusinessId('helloworld-4')).to.be.eq(false);
+			expect(isValidBusinessId(null)).to.be.eq(false);
+			expect(isValidBusinessId(undefined)).to.be.eq(false);
 		});
 		it('should validate old business ID', () => {
 			expect(isValidBusinessId('737546-2')).to.be.eq(true);
@@ -32,7 +47,7 @@ describe('test business ID', () => {
 			expect(() => getMultiplier(-1)).to.throw(RangeError, 'Index out of bounds');
 		});
 		it('should test getBaseId function', () => {
-			expect(() => getBaseId('')).to.throw(Error, 'not valid business id');
+			expect(() => getBaseId('' as `${number}-${number}`)).to.throw(Error, 'is not valid Finnish Business ID');
 			expect(getBaseId('1572860-0')).to.be.eq('1572860');
 		});
 	});
